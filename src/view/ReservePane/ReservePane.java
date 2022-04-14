743b9710f87364cf5fb299056edbf4845db69a8a
FLAiRistaken <flairx@protonmail.com>
2022-04-14 14:33:39 +0100

    Started to add resize capabilities to ReservePane

--- src/view/ReservePane/ReservePane.java
+++ src/view/ReservePane/ReservePane.java
@@ -4,7 +4,9 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Accordion;
 import javafx.scene.control.TitledPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 
 public class ReservePane extends Accordion {
@@ -29,17 +31,31 @@ public class ReservePane extends Accordion {
         unTerm1.getLblList().setText("Unselected Term 1 modules");
         resTerm1.getLblList().setText("Reserved Term 1 modules");
 
-        HBox term1Hbox = new HBox(unTerm1, resTerm1);
+       /* HBox term1Hbox = new HBox(unTerm1, resTerm1);
         term1Hbox.setSpacing(10);
         term1Hbox.setPadding(new Insets(20));
-        term1Hbox.setAlignment(Pos.CENTER);
+        term1Hbox.setAlignment(Pos.CENTER);*/
+        GridPane term1GridPane = new GridPane();
+
+        term1GridPane.add(unTerm1, 0, 1);
+        term1GridPane.add(resTerm1, 1, 1);
+
+        term1GridPane.setPadding(new Insets(20));
+        term1GridPane.setAlignment(Pos.CENTER);
+
+        term1GridPane.setGridLinesVisible(true);
+
+        unTerm1.prefWidthProperty().bind(this.widthProperty());
+        resTerm1.prefWidthProperty().bind(this.widthProperty());
+        unTerm1.prefHeightProperty().bind(this.heightProperty());
+        resTerm1.prefHeightProperty().bind(this.heightProperty());
 
         btnTerm1 = new ButtonsPane();
         btnTerm1.getLblButton().setText("Reserve 30 credits worth of term 1 modules");
         btnTerm1.setPadding(new Insets(20));
         btnTerm1.setAlignment(Pos.CENTER);
 
-        VBox term1Vbox = new VBox(term1Hbox, btnTerm1);
+        VBox term1Vbox = new VBox(term1GridPane, btnTerm1);
 
         term1Pane = new TitledPane("Term 1 modules", term1Vbox);
 
@@ -54,6 +70,11 @@ public class ReservePane extends Accordion {
         term2Hbox.setPadding(new Insets(20));
         term2Hbox.setAlignment(Pos.CENTER);
 
+        unTerm2.prefWidthProperty().bind(this.widthProperty());
+        resTerm2.prefWidthProperty().bind(this.widthProperty());
+        unTerm2.prefHeightProperty().bind(term2Hbox.heightProperty());
+        resTerm2.prefHeightProperty().bind(term2Hbox.heightProperty());
+
         btnTerm2 = new ButtonsPane();
         btnTerm2.getLblButton().setText("Reserve 30 credits worth of term 2 modules");
         btnTerm2.setPadding(new Insets(20));
@@ -65,6 +86,7 @@ public class ReservePane extends Accordion {
 
         this.getPanes().addAll(term1Pane, term2Pane);
         this.setExpandedPane(term1Pane);
+
 
 
     }
